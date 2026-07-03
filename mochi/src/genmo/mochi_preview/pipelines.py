968149b439d49319eeb7a72734b6149d77251420
박혜림 <parkhyellim@baghyelim-ui-MacBookAir.local>
2026-07-04 05:14:41 +0900

    Allow overriding Mochi T5 model path

--- mochi/src/genmo/mochi_preview/pipelines.py
+++ mochi/src/genmo/mochi_preview/pipelines.py
@@ -67,7 +67,7 @@ def linear_quadratic_schedule(num_steps, threshold_noise, linear_steps=None):
     return sigma_schedule
 
 
-T5_MODEL = "google/t5-v1_1-xxl"
+T5_MODEL = os.environ.get("MOCHI_T5_MODEL", "google/t5-v1_1-xxl")
 MAX_T5_TOKEN_LENGTH = 256
 
 
